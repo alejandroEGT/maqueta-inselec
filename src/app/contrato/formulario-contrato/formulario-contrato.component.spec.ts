0b7eeb157300c353c79f7d7cf7644698ea169747
davidluna1995 <david.luna.cuevas@gmail.com>
2021-07-08 14:24:59 -0400

    subida de formulario contrato

---
+++ src/app/contrato/formulario-contrato/formulario-contrato.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormularioContratoComponent } from './formulario-contrato.component';
+
+describe('FormularioContratoComponent', () => {
+  let component: FormularioContratoComponent;
+  let fixture: ComponentFixture<FormularioContratoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FormularioContratoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormularioContratoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
